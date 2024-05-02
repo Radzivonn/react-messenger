c57f858d64c777679e57b18b153f7e18740997e5
Rodion <rodionserez@gmail.com>
2024-05-02 22:10:58 +0300

    feat: change user router path

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: '*' }));
-app.use('/auth', userRouter);
+app.use('/user', userRouter);
 app.use(errorMiddleware);
 
 const httpServer = createServer(app);
