7623353184a879f61bc57956110d055f2fcdc947
Rodion <rodionserez@gmail.com>
2024-05-09 22:12:49 +0300

    feat: sync server's validations with the client's

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import userController from '../controllers/user-controller.js';
 import { body } from 'express-validator';
 import { authMiddleware } from '../middlewares/auth-middleware.js';
+
 const userRouter = Router();
 
 userRouter.post(
   '/registration',
   body('email').isEmail(),
-  body('password').isLength({ min: 6, max: 32 }),
+  body('password').isLength({ min: 8, max: 24 }),
   userController.registration,
 );
 userRouter.post('/login', userController.login);
