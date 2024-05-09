89160262b356a59cf06bd0a5a55f545cdce5bcb5
Rodion <rodionserez@gmail.com>
2024-05-09 22:12:08 +0300

    feat: configure CORS

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use('/user', userRouter);
 app.use(errorMiddleware);
 
