b81a57623058a17160147433e6b1e09906709dda
Rodion <rodionserez@gmail.com>
2024-07-08 19:44:35 +0300

    feat: rework socket logic for tracking online statuses of users

--- src/service/socket-service.ts
+++ src/service/socket-service.ts
@@ -1,38 +1,62 @@
 import { Server } from 'socket.io';
 import { ClientToServerEvents, ServerToClientEvents, WEBSOCKET_EVENTS } from '../types/types.js';
 import { chatService } from './chat-service.js';
+import { userService } from './user-service.js';
+import { friendListService } from './friend-list-service.js';
 
 const startSocketServer = (io: Server<ClientToServerEvents, ServerToClientEvents>) => {
-  io.on(WEBSOCKET_EVENTS.CONNECTION, (socket) => {
-    socket.on(WEBSOCKET_EVENTS.JOIN_ROOM, async ({ chatId, userId, receiverId }) => {
-      const [chat, isCreated] = await chatService.addChat(chatId, userId, receiverId);
-      const isReceiverOnline = (await io.in(chatId).fetchSockets()).length > 0;
+  io.on(WEBSOCKET_EVENTS.CONNECTION, async (socket) => {
+    socket.on(WEBSOCKET_EVENTS.CONNECT_PARTICIPANT, async (userId, userName) => {
+      await userService.changeOnlineStatus(userId, true);
+      const chats = await chatService.getUserChats(userId, userName);
 
-      socket.join(chatId);
-      socket.emit(WEBSOCKET_EVENTS.JOINED_ROOM_SUCCESSFULLY, {
-        messages: chat.messages,
-        isCreated,
+      chats.forEach((chat) => {
+        socket.join(chat.chatId);
+        socket.to(chat.chatId).emit(WEBSOCKET_EVENTS.PARTICIPANT_CONNECTED, userId);
       });
 
-      io.to(chatId).emit(WEBSOCKET_EVENTS.CONNECT_PARTICIPANT, isReceiverOnline);
+      const onlineStatusesArray = await friendListService.getFriendsOnlineStatuses(userId);
+
+      const onlineStatuses = onlineStatusesArray.reduce<Record<string, boolean>>(
+        (object, value) => {
+          return { ...object, [value.userId]: value.online };
+        },
+        {},
+      );
+
+      socket.emit(WEBSOCKET_EVENTS.SOCKET_SUCCESSFULLY_CONNECTED, onlineStatuses);
+    });
+
+    socket.on(WEBSOCKET_EVENTS.DISCONNECT_PARTICIPANT, async (userId) => {
+      await userService.changeOnlineStatus(userId, false);
+
+      for (const room of socket.rooms) {
+        if (room !== socket.id) {
+          socket.leave(room);
+          socket.to(room).emit(WEBSOCKET_EVENTS.PARTICIPANT_DISCONNECTED, userId);
+        }
+      }
+
+      socket.emit(WEBSOCKET_EVENTS.SOCKET_SUCCESSFULLY_DISCONNECTED);
+    });
+
+    socket.on(WEBSOCKET_EVENTS.JOIN_ROOM, async ({ chatId, userId, receiverId }) => {
+      const [chat, isCreated] = await chatService.addChat(chatId, userId, receiverId);
+
+      if (isCreated) socket.join(chat.chatId);
+
+      socket.emit(WEBSOCKET_EVENTS.JOINED_ROOM_SUCCESSFULLY, {
+        chat,
+        isCreated,
+      });
     });
 
     socket.on(WEBSOCKET_EVENTS.SEND_MESSAGE, async (message) => {
       await chatService.saveMessages(message.chatId, [message]);
       io.to(message.chatId).emit(WEBSOCKET_EVENTS.RECEIVE_MESSAGE, message);
     });
-
-    socket.on(WEBSOCKET_EVENTS.LEAVE_ROOM, (chatId) => {
-      socket.leave(chatId);
-      socket.removeAllListeners();
-      io.to(chatId).emit(WEBSOCKET_EVENTS.DISCONNECT_PARTICIPANT);
-    });
-
-    socket.on(WEBSOCKET_EVENTS.DISCONNECTING, () => {
-      const chatId = Array.from(socket.rooms)[1]; // take current chat id from rooms Set (second element because the first index contains some service id)
-      io.to(chatId).emit(WEBSOCKET_EVENTS.DISCONNECT_PARTICIPANT);
-    });
   });
+
   io.on(WEBSOCKET_EVENTS.DISCONNECT, (reason) => {
     console.log(`Server disconnected by reason: ${reason}`);
   });
