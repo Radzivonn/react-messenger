1ebd4e4dd3e066dcaafbdc100eb1aaaed0bf6db7
Rodion <rodionserez@gmail.com>
2024-09-25 22:08:04 +0300

    refactor: renaming socket events

--- src/service/socket-service.ts
+++ src/service/socket-service.ts
@@ -6,13 +6,13 @@ import { friendListService } from './friend-list-service.js';
 
 const startSocketServer = (io: Server<ClientToServerEvents, ServerToClientEvents>) => {
   io.on(WEBSOCKET_EVENTS.CONNECTION, async (socket) => {
-    socket.on(WEBSOCKET_EVENTS.CONNECT_PARTICIPANT, async (userId, userName) => {
+    socket.on(WEBSOCKET_EVENTS.CONNECT_USER, async (userId, userName) => {
       await userService.changeOnlineStatus(userId, true);
       const chats = await chatService.getUserChats(userId, userName);
 
       chats.forEach((chat) => {
         socket.join(chat.chatId);
-        socket.to(chat.chatId).emit(WEBSOCKET_EVENTS.PARTICIPANT_CONNECTED, userId);
+        socket.to(chat.chatId).emit(WEBSOCKET_EVENTS.USER_CONNECTED, userId);
       });
 
       const onlineStatusesArray = await friendListService.getFriendsOnlineStatuses(userId);
@@ -24,28 +24,28 @@ const startSocketServer = (io: Server<ClientToServerEvents, ServerToClientEvents
         {},
       );
 
-      socket.emit(WEBSOCKET_EVENTS.SOCKET_SUCCESSFULLY_CONNECTED, onlineStatuses);
+      socket.emit(WEBSOCKET_EVENTS.SOCKET_SUCCESSFULLY_CONNECTED, onlineStatuses, chats);
     });
 
-    socket.on(WEBSOCKET_EVENTS.DISCONNECT_PARTICIPANT, async (userId) => {
+    socket.on(WEBSOCKET_EVENTS.DISCONNECT_USER, async (userId) => {
       await userService.changeOnlineStatus(userId, false);
 
       for (const room of socket.rooms) {
         if (room !== socket.id) {
           socket.leave(room);
-          socket.to(room).emit(WEBSOCKET_EVENTS.PARTICIPANT_DISCONNECTED, userId);
+          socket.to(room).emit(WEBSOCKET_EVENTS.USER_DISCONNECTED, userId);
         }
       }
 
       socket.emit(WEBSOCKET_EVENTS.SOCKET_SUCCESSFULLY_DISCONNECTED);
     });
 
-    socket.on(WEBSOCKET_EVENTS.JOIN_ROOM, async ({ chatId, userId, receiverId }) => {
+    socket.on(WEBSOCKET_EVENTS.CREATE_CHAT, async ({ chatId, userId, receiverId }) => {
       const [chat, isCreated] = await chatService.addChat(chatId, userId, receiverId);
 
       if (isCreated) socket.join(chat.chatId);
 
-      socket.emit(WEBSOCKET_EVENTS.JOINED_ROOM_SUCCESSFULLY, {
+      socket.emit(WEBSOCKET_EVENTS.CREATED_CHAT_SUCCESSFULLY, {
         chat,
         isCreated,
       });
