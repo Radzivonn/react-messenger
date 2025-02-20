f32994ec4c146e1eb066212b13b8de0e895d874e
Rodion <rodionserez@gmail.com>
2025-02-20 23:20:04 +0300

    fix response header

--- src/middlewares/CORP-middleware.ts
+++ src/middlewares/CORP-middleware.ts
@@ -3,6 +3,6 @@ import { Request, Response, NextFunction } from 'express';
 export const CORPmiddleware = (req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-  res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   next();
 };
